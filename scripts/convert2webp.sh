a7ef8696aa886707454eb55d03663b77898834cd
Kong Jun <kongjun18@outlook.com>
2022-10-26 16:13:52 +0800

    only try convert new-pushed image to webp

--- scripts/convert2webp.sh
+++ scripts/convert2webp.sh
@@ -2,12 +2,22 @@
 # Convert image to webp format!
 # Install cwebp via `apt-get install -y webp`.
 
+set -e
+set -o pipefail
+
 convert2webp() {
     for img in $1; do
         local src="${img}"
         local dst="public/${src#*/}"
-        cwebp "${src}" -o "${dst}.webp"
-        echo -e "convert ${src} to ${dst}.webp\n"
+        # Only convert to webp when webp image not exists or out-of-date.
+        if [[ ! -e "${dst}.webp" ]] || [[ $(${GIT} log --format=%ct -1 "${src}") -gt $(stat --format=%Y "${dst}.webp") ]]; then
+            if cwebp "${src}" -o "${dst}.webp"; then
+                echo "converted ${src} to ${dst}.webp"
+            else
+                echo "error in convert ${src} to ${dst}.webp" > /dev/stderr
+            fi
+        fi
+        echo "image ${dst}.webp is up-to-date."
     done
 }
 
