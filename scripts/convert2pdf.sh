f551c815590c37a8ee5ac13db4ea87ed0789ddf9
Kong Jun <kongjun18@outlook.com>
2022-10-26 21:00:19 +0800

    update pdf archives on demand

--- scripts/convert2pdf.sh
+++ scripts/convert2pdf.sh
@@ -6,11 +6,16 @@ MARKDWN2PDF="$(realpath ./scripts/markdown2pdf)"
 for post in public/posts/*; do
   if [[ -d "${post}" ]] && [[ -e "${post}/index.html" ]]; then
     index_md="content/${post#public/}/index.md"
+    post_dir="${index_md%index.md}"
     date="$(head -n 10 "${index_md}" | grep date | cut -d ' ' -f 2)"
     date="$(date -d "${date}" +'%Y-%m-%d')"
     archived_pdf="${archive_dir}/${post##*/}.pdf"
-    echo "converting ${index_md} to ${archived_pdf}"
-    archived_pdf="$(realpath "${archived_pdf}")"
-    (cd "${index_md%index.md}" && ${MARKDWN2PDF} -i index.md -d "${date}" -o "${archived_pdf}")
+    if [[ ! -e "${archived_pdf}" ]] || [[ $(${GIT} log --format=%ct -1 "${post_dir}") -gt $(stat --format=%Y "${archived_pdf}") ]]; then
+      echo "converting ${index_md} to ${archived_pdf}"
+      archived_pdf="$(realpath "${archived_pdf}")"
+      (cd "${post_dir}" && ${MARKDWN2PDF} -i index.md -d "${date}" -o "${archived_pdf}")
+    else
+      echo "${archive_dir} is up-to-date."
+    fi
   fi
 done
