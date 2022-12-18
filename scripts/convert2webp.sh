bfa15c5f665fb3b5f55a48f345aaa747d8d7eaff
Kong Jun <kongjun18@outlook.com>
2022-12-18 19:49:48 +0800

    maintain archive index manually

--- scripts/convert2webp.sh
+++ scripts/convert2webp.sh
@@ -8,13 +8,25 @@ set -o pipefail
 convert2webp() {
     for img in $1; do
         local src="${img}"
-        local dst="public/${src#*/}"
-        # Only convert to webp when webp image not exists or out-of-date.
-        if [[ ! -e "${dst}.webp" ]] || [[ $(${GIT} log --format=%ct -1 "${src}") -gt $(stat --format=%Y "${dst}.webp") ]]; then
-            echo "converting ${src} to ${dst}.webp"
-            cwebp "${src}" -o "${dst}.webp"
+        local post
+        # content/posts/<post>/images/<image>
+        post="$(dirname "$(dirname "${img}")")"
+        local post_md="${post}/index.md"
+        # Check whether the image path is right
+        if [[ -e "${post_md}" ]]; then
+            local date
+            # Convert date like 2021-04-28T15:16:23+08:00 to 2021/04/28
+            date="$(head -n 10 "${post_md}" | grep 'date' - | cut -d ' ' -f 2 | cut -d 'T' -f 1 | sed 's/-/\//g')"
+            # public/posts/<date>images/<image>
+            local dst
+            dst="public/posts/${date}/images/$(basename "${img}")"
+            # Only convert to webp when webp image not exists or out-of-date.
+            if [[ ! -e "${dst}.webp" ]] || [[ $(${GIT} log --format=%ct -1 "${src}") -gt $(stat --format=%Y "${dst}.webp") ]]; then
+                echo "converting ${src} to ${dst}.webp"
+                cwebp "${src}" -o "${dst}.webp"
+            fi
+            echo "image ${dst}.webp is up-to-date."
         fi
-        echo "image ${dst}.webp is up-to-date."
     done
 }
 
